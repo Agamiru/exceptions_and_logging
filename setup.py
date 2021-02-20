ceff3f42859f578d2ebc4597299c7f32e6a19949
Agamiru <chidinnaemekannadi@gmail.com>
2021-02-20 17:15:20 +0100

    check

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="exceptions_and_logging",
-    version="0.0.1",
+    version="0.0.2",
     author="Chidi Nnadi",
     author_email="chidinnaemekannadi@gmail.com",
     description="A Django app for logging custom application errors",
